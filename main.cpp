2adc1e8c671f413eb836baece317755af8ec8a82
FXHibon <djxf44@gmail.com>
2014-12-02 09:13:19 +0100

    Main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Welcome into our next generation game!" << endl;
+    cout << "This is awesome!";
+
+    int val;
+    cin >> val;
     return 0;
 }
