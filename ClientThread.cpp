f23cd4e145c63cd7b85bced5c2e33c8be024b236
MD4 <m.dequa@gmail.com>
2015-02-10 11:04:09 +0100

    Commit du BONHEUR.

--- ClientThread.cpp
+++ ClientThread.cpp
@@ -1,9 +1,8 @@
 #include <winsock2.h>
-#include <windef.h>
 #include <iostream>
 #include "ServerException.hpp"
 #include "Server.hpp"
-#include "ClientThread.hpp"
+
 using namespace std;
 
 ClientThread::ClientThread() {
@@ -14,17 +13,21 @@ ClientThread::ClientThread(int clientID, SOCKET socket, Server *server) {
     this->clientID = clientID;
     this->socket = socket;
     this->server = server;
+    this->state = ClientState::HELLO;
 }
 
 DWORD ClientThread::run() {
     char buffer[50];
+    bool activeClient = true;
 
     cout << "thread client démarré" << endl;
 
     int length = 1;
-    boolean endWhile = false;
+    bool endWhile = false;
 
-    while (true) {
+    this->sendMessage("hello");
+
+    while (activeClient) {
         length = recv(this->socket, buffer, 50, 0);
 
         if (length == -1) {
@@ -39,25 +42,24 @@ DWORD ClientThread::run() {
         string instr = message.substr(0, 4);
         string data = message.substr(4, length - 4);
 
-
-        if (instr == "auth") {
-
-            this->pseudo = data;
-            this->server->notifyToAll(this->pseudo + " connected !");
-
-            cout << this->pseudo << " connected " << endl;
-
-        } else if (instr == "mssg") {
-
-            string message = bufferToString(buffer, length);
-            cout << this->pseudo << " says : " << data << endl;
-            this->server->notifyToAll(this->pseudo + " says : " + data);
-
-        } else if (instr == "exit") {
-
-            break;
-
+        if (instr == "exit") {
+            this->state = ClientState::BYE;
         }
+
+        switch (this->state) {
+            case ClientState::HELLO:
+                this->state = this->handleHello(instr, data);
+                break;
+
+            case ClientState::NICE_2_MEET_YOU:
+                this->state = this->handleNice2MeetYou(instr, data);
+                break;
+
+            case ClientState::BYE:
+                activeClient = false;
+                break;
+        }
+
     }
 
     cout << "client socket closed" << endl;
@@ -69,7 +71,7 @@ void ClientThread::sendMessage(string message) {
     send(this->socket, message.c_str(), (int) strlen(message.c_str()), 0);
 }
 
-std::string ClientThread::bufferToString(char* buffer, int bufflen) {
+std::string ClientThread::bufferToString(char *buffer, int bufflen) {
     std::string ret(buffer, bufflen);
     return ret;
 }
@@ -80,4 +82,39 @@ int ClientThread::getClientID() {
 
 string ClientThread::getPseudo() {
     return this->pseudo;
+}
+
+ClientState ClientThread::handleHello(string instr, string data) {
+    if (instr == "auth") {
+
+        if (data.length() >= 1) {
+
+            this->pseudo = data;
+            this->server->notifyToAll(this->pseudo + " connected !");
+
+            cout << this->pseudo << " connected " << endl;
+
+            this->sendMessage("ntmy");
+
+            return ClientState::NICE_2_MEET_YOU;
+
+
+        }
+
+    }
+    this->sendMessage("argh");
+    return this->state;
+}
+
+ClientState ClientThread::handleNice2MeetYou(string instr, string data) {
+    if (instr == "mssg") {
+
+        cout << this->pseudo << " says : " << data << endl;
+
+        this->server->notifyToAll(this->pseudo + " says : " + data);
+
+    }
+
+    this->sendMessage("argh");
+    return this->state;
 }
--- ClientThread.hpp
+++ ClientThread.hpp
@@ -1,5 +1,6 @@
 #ifndef _CLIENT_THREAD
 #define _CLIENT_THREAD
+
 #include <winsock2.h>
 #include <windef.h>
 #include <iostream>
@@ -10,24 +11,42 @@ using namespace std;
 
 class Server;
 
+enum ClientState {
+    HELLO,
+    NICE_2_MEET_YOU,
+    BYE
+};
+
 class ClientThread {
 
 public:
     ClientThread();
+
     ClientThread(int clientID, SOCKET socket, Server *server);
 
     void sendMessage(string message);
+
     DWORD run();
 
     int getClientID();
+
     string getPseudo();
+
 private:
+
+
     int clientID;
     SOCKET socket;
     Server *server;
 
     string pseudo;
+    ClientState state;
 
-    std::string bufferToString(char* buffer, int bufflen);
+    std::string bufferToString(char *buffer, int bufflen);
+
+    ClientState handleHello(string instr, string data);
+
+    ClientState handleNice2MeetYou(string instr, string data);
 };
+
 #endif
